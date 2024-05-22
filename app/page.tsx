4656d1f2fd4d0141dada75b84af7a295581ace3e
Leonardo Montes <leo@Leonardos-MacBook-Air.local>
2024-05-21 22:29:02 -0500

    Added Google login

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,26 @@
 "use client";
-import { useRouter } from "next/navigation";
-import LoginForm from "./components/LoginForm";
-import Logo from "./components/Logo";
 
-export default function Home() {
+import { useRouter } from "next/navigation";
+import SignIn from "./components/sign-in";
+
+import NavBar from "./components/NavBar";
+
+export default function Landing() {
   const router = useRouter();
   return (
-    <main className="h-screen flex flex-col">
-      <Logo />
-      <button onClick={() => router.push("/login")}>Login</button>
-      <button onClick={() => router.push("/")}>Sign Up</button>
+    <main className="h-dvh w-dvh flex flex-col gap-[150px]">
+      <NavBar />
+      <div className="flex flex-col items-center gap-[20px]">
+        <div className="flex flex-col items-center">
+          <div className="font-semibold text-[89.76px] bg-gradient-to-r from-[#4356ff] to-[#283499] bg-clip-text text-transparent">
+            PhysNova
+          </div>
+          <div className="text-[37.9px]">
+            Mastering IB Physics 5 questions at a time
+          </div>
+        </div>
+        <SignIn />
+      </div>
     </main>
   );
 }
