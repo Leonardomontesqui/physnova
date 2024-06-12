edb391e842b9062b2d8412372d934763ffdc77e0
Marcelo ChamanMallqui <marcechaman@gmail.com>
2024-06-11 21:59:14 -0400

    fixing api

---
+++ app/components/CorrectAnswers.1.tsx
@@ -0,0 +1,163 @@
+import React, {useEffect, useState} from 'react';
+import {QUESTION_LIST} from '../../lib/constants';
+import ReactMarkdown from 'react-markdown';
+import remarkMath from 'remark-math';
+import remarkGfm from 'remark-gfm';
+import rehypeKatex from 'rehype-katex';
+import rehypeRaw from 'rehype-raw';
+import 'katex/dist/katex.min.css';
+import {supabaseBrowser} from '@/lib/supabase/browser';
+import {ChevronLeft, ChevronRight} from 'lucide-react';
+
+const supabase = supabaseBrowser();
+
+interface option {
+  text: string;
+  isCorrect: boolean;
+}
+export default function CorrectAnswers() {
+  const [indexSet, setIndexSet] = useState<number[]>([]);
+  const [currentIndex, setCurrentIndex] = useState<number>(1);
+  const [currentQuestion, setCurrentQuestion] = useState<any>();
+  const [clickedOptionList, setClickedOptionList] = useState<number[]>([]);
+
+  useEffect(() => {
+    fetchQuestionIndex();
+  }, []);
+
+  useEffect(() => {
+    if (indexSet?.length > 0) {
+      setCurrentQuestion(QUESTION_LIST[indexSet[1]]);
+    }
+  }, [indexSet]);
+
+  const fetchQuestionIndex = async () => {
+    const {data: userDataRes, error: userDataError} =
+      await supabase.auth.getUser();
+
+    if (!userDataRes || userDataError) {
+      console.error('Error fetching user data');
+      return;
+    }
+
+    const {data: gameplayDataRes, error: gameplayDataError} = await supabase
+      .from('gameplay')
+      .select('question_index_list')
+      .eq('user_id', userDataRes.user.id);
+
+    if (!gameplayDataRes || gameplayDataError) {
+      console.error('Error fetching gameplay data');
+      return;
+    } else {
+      const latestGameData = gameplayDataRes[gameplayDataRes.length - 1];
+      setIndexSet(latestGameData.question_index_list);
+    }
+
+    //latest option index data
+    const {data: optionIndex, error: optionIndexError} = await supabase
+      .from('gameplay')
+      .select('option_index_list')
+      .eq('user_id', userDataRes.user.id);
+
+    if (!optionIndex || optionIndexError) {
+      console.error('Error fetching option index data');
+      return;
+    } else {
+      const latestOptionData = optionIndex[optionIndex.length - 1];
+      setClickedOptionList(latestOptionData.option_index_list);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (currentIndex < 5) {
+      const newIndex = currentIndex + 1;
+      setCurrentIndex(newIndex);
+      setCurrentQuestion(QUESTION_LIST[indexSet[newIndex]]);
+    }
+  };
+
+  const handlePrevClick = () => {
+    if (currentIndex > 1) {
+      const newIndex = currentIndex - 1;
+      setCurrentIndex(newIndex);
+      setCurrentQuestion(QUESTION_LIST[indexSet[newIndex]]);
+    }
+  };
+
+  const getOptionColor = (index: number, isCorrect: boolean) => {
+    if (index == clickedOptionList[currentIndex - 1]) {
+      return isCorrect
+        ? 'border-[#d2e9d8] bg-[#ecf8ef]'
+        : 'border-[#e7cccc] bg-[#fde2e2]';
+    }
+    if (isCorrect) {
+      return 'border-[#d2e9d8] bg-[#ecf8ef]';
+    } else {
+      return 'border-[#e0e0e0]';
+    }
+  };
+
+  return (
+    <div className='w-full h-full min-h-0 bg-white rounded-3xl border border-[#d9d9d9] p-[24px] flex flex-col items-center gap-[8px] basis-2/3'>
+      <div className='font-medium'>Answers</div>
+      <div className='h-full min-h-0 w-full border rounded-3xl bg-white flex flex-col px-[32px] py-[16px] gap-[16px]'>
+        <div className='text-[#bfbfbf] text-[14px]'>{currentIndex} of 5</div>
+        <div className='flex min-h-0 h-full flex-col gap-[8px] min-h-0 overflow-y-scroll no-scrollbar'>
+          <div>
+            {currentQuestion?.Question && (
+              <ReactMarkdown
+                className='text-[14px]'
+                remarkPlugins={[remarkMath, remarkGfm]}
+                rehypePlugins={[rehypeKatex, rehypeRaw]}
+              >
+                {currentQuestion.Question}
+              </ReactMarkdown>
+            )}
+          </div>
+          <div className='max-h-[140px]'>
+            {currentQuestion?.Image && (
+              <img
+                className='mx-auto h-[100%]'
+                src={currentQuestion.Image}
+                alt='Image Related to Question'
+              />
+            )}
+          </div>
+        </div>
+        <div className='flex flex-col gap-[8px] '>
+          {currentQuestion?.Options &&
+            currentQuestion?.Options.map((option: option, index: number) => (
+              <div
+                key={option.text}
+                className={`border rounded-lg px-[16px] py-[8px] text-left ${getOptionColor(
+                  index,
+                  option.isCorrect
+                )}`}
+              >
+                <ReactMarkdown
+                  remarkPlugins={[remarkMath, remarkGfm]}
+                  rehypePlugins={[rehypeKatex, rehypeRaw]}
+                >
+                  {option.text}
+                </ReactMarkdown>
+              </div>
+            ))}
+        </div>
+      </div>
+      <div className='flex justify-between w-full'>
+        <button
+          className='border border-[#d0cece] mx-2 rounded px-[2px] py-[2px]'
+          onClick={() => handlePrevClick()}
+        >
+          <ChevronLeft size={20} />
+        </button>
+        <button
+          className='border border-[#d0cece] mx-2 rounded px-[2px] py-[2px]'
+          onClick={() => handleNextClick()}
+        >
+          <ChevronRight size={20} />
+        </button>
+      </div>
+    </div>
+  );
+}
--- app/components/QuestionCard.tsx
+++ app/components/QuestionCard.tsx
@@ -1,9 +1,21 @@
 'use client';
 import React, {useState, useEffect} from 'react';
+<<<<<<< Updated upstream
 import {questionList} from '../questionList';
 import {supabaseBrowser} from '@/lib/supabase/browser';
 import {User} from '@supabase/supabase-js';
 import {useRouter} from 'next/navigation';
+=======
+import {QUESTION_LIST} from '../../lib/constants';
+import {supabaseBrowser} from '@/lib/supabase/browser';
+import {useRouter} from 'next/navigation';
+import ReactMarkdown from 'react-markdown';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
+import rehypeRaw from 'rehype-raw';
+import remarkGfm from 'remark-gfm';
+import 'katex/dist/katex.min.css';
+>>>>>>> Stashed changes
 
 const supabase = supabaseBrowser();
 
@@ -12,7 +24,7 @@ export default function QuestionCard() {
   const [currentIndex, setCurrentIndex] = useState<number | null>(null); // Current question index
   const [correctAnswers, setCorrectAnswers] = useState<number>(0); // Counter for correct answers
   const currentQuestion =
-    currentIndex !== null ? questionList[currentIndex] : null;
+    currentIndex !== null ? QUESTION_LIST[currentIndex] : null;
   const router = useRouter();
 
   useEffect(() => {
@@ -22,20 +34,53 @@ export default function QuestionCard() {
   const insertGameplayData = async (isLastCorrect: boolean) => {
     const supabase = supabaseBrowser();
 
+<<<<<<< Updated upstream
     const {data: userDataRes, error: userDataError} =
       await supabase.auth.getUser();
 
     if (!userDataRes || userDataError) {
+=======
+  const getNextUniqueIndex = () => {
+    let newIndex;
+    do {
+      newIndex = Math.floor(Math.random() * QUESTION_LIST.length);
+    } while (questionIndexSet.includes(newIndex));
+
+    setQuestionIndexSet((prev) => [...prev, newIndex]);
+    setCurrentIndex(newIndex);
+  };
+
+  const insertGamePlayData = async (index: number, isLastCorrect: boolean) => {
+    let updatedIndexSelect = optionClickedIndex;
+    updatedIndexSelect.push(index);
+    const {data: userData, error: userDataError} =
+      await supabase.auth.getUser();
+
+    if (!userData || userDataError) {
+>>>>>>> Stashed changes
       console.error('Error fetching user data');
       return;
     }
 
+<<<<<<< Updated upstream
     const {error} = await supabase.from('gameplay').insert({
       accurate: correctAnswers + Number(isLastCorrect),
       name: userDataRes.user.user_metadata.name,
     });
 
     if (error) {
+=======
+    const {error: gameplayInsertError} = await supabase
+      .from('gameplay')
+      .insert({
+        accurate: correctAnswers + Number(isLastCorrect),
+        name: userData.user.user_metadata.name,
+        question_index_list: questionIndexSet,
+        option_index_list: updatedIndexSelect,
+      });
+
+    if (gameplayInsertError) {
+>>>>>>> Stashed changes
       console.error('Error inserting gameplay data');
     }
   };
@@ -58,12 +103,17 @@ export default function QuestionCard() {
     if (currentIndexSet.length <= 5) {
       getNextUniqueIndex();
     } else {
+<<<<<<< Updated upstream
       insertGameplayData(option.isCorrect as boolean);
+=======
+      insertGamePlayData(index, option.isCorrect);
+>>>>>>> Stashed changes
       router.push('/home');
     }
   };
 
   return (
+<<<<<<< Updated upstream
     <div className='h-full w-full border rounded-3xl bg-white flex flex-col px-[60px] py-[40px] justify-between'>
       <div className='flex flex-col gap-[32px]'>
         <div className='text-[#bfbfbf]'>{currentIndexSet.length - 1} of 5</div>
@@ -81,6 +131,51 @@ export default function QuestionCard() {
               {option.text}
             </button>
           ))}
+=======
+    <div className='min-h-full h-full w-full border rounded-3xl bg-white flex flex-col px-[64px] py-[20px] gap-[16px]'>
+      <div className='text-[#bfbfbf] text-[14px]'>
+        {questionIndexSet.length - 1} of 5
+      </div>
+      <div className='flex flex-col gap-[16px] h-full'>
+        <div>
+          {currentQuestion?.Question && (
+            <ReactMarkdown
+              remarkPlugins={[remarkMath, remarkGfm]}
+              rehypePlugins={[rehypeKatex, rehypeRaw]}
+            >
+              {currentQuestion.Question}
+            </ReactMarkdown>
+          )}
+        </div>
+
+        {currentQuestion?.Image && (
+          <img
+            className='mx-auto max-h-[200px]'
+            src={currentQuestion.Image}
+            alt='Image Related to Question'
+          />
+        )}
+      </div>
+      <div className='flex flex-col gap-[8px]'>
+        {currentQuestion?.Options &&
+          shuffledOptionIndices.map((shuffledIndex) => {
+            const option = currentQuestion.Options[shuffledIndex];
+            return (
+              <button
+                key={shuffledIndex}
+                className='border rounded-lg px-[16px] py-[8px] text-left hover:bg-[#f7f7f7] after:bg-[#4356ff]'
+                onClick={() => handleOptionClick(shuffledIndex, option)}
+              >
+                <ReactMarkdown
+                  remarkPlugins={[remarkMath, remarkGfm]}
+                  rehypePlugins={[rehypeKatex, rehypeRaw]}
+                >
+                  {option.text}
+                </ReactMarkdown>
+              </button>
+            );
+          })}
+>>>>>>> Stashed changes
       </div>
     </div>
   );
