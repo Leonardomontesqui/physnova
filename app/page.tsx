fa87c8ed8501dcc003403cdb544b0027af731062
Marcelo ChamanMallqui <marcechaman@gmail.com>
2024-06-11 22:00:55 -0400

    npm audit fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,14 @@
 'use client';
-<<<<<<< Updated upstream
-
 import {supabaseBrowser} from '@/lib/supabase/browser';
 import NavBar from './components/NavBar';
-
-=======
-import {supabaseBrowser} from '@/lib/supabase/browser';
-import NavBar from './components/NavBar';
->>>>>>> Stashed changes
 const supabase = supabaseBrowser();
 
 export default function Landing() {
   const handleLoginWithOAuth = async () => {
-<<<<<<< Updated upstream
-    supabase.auth.signInWithOAuth({
-      provider: 'google',
-      options: {
-        redirectTo: window.location.origin + '/auth/callback?next=/home',
-=======
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
         redirectTo: `${window.location.origin}/api/auth/callback?next=/home`,
->>>>>>> Stashed changes
       },
     });
   };
@@ -32,11 +18,7 @@ export default function Landing() {
       <NavBar />
       <div className='flex flex-col items-center gap-[20px]'>
         <div className='flex flex-col items-center'>
-<<<<<<< Updated upstream
-          <div className='font-s`emibold text-[89.76px] bg-gradient-to-r from-[#4356ff] to-[#283499] bg-clip-text text-transparent'>
-=======
           <div className='font-semibold text-[89.76px] bg-gradient-to-r from-[#4356ff] to-[#283499] bg-clip-text text-transparent'>
->>>>>>> Stashed changes
             PhysNova
           </div>
           <div className='text-3xl'>
