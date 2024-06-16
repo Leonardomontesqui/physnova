4a194fb4128c6ecfb54b4d5f37cec165b8608b12
Leonardo Montes <leo@Leonardos-MacBook-Air.local>
2024-06-16 00:25:15 -0500

    added demo video

--- app/page.tsx
+++ app/page.tsx
@@ -14,23 +14,27 @@ export default function Landing() {
   };
 
   return (
-    <main className="h-dvh w-dvh flex flex-col gap-[150px]">
+    <main className="h-dvh w-dvh flex flex-col">
       <NavBar />
-      <div className="flex flex-col items-center gap-[20px]">
-        <div className="flex flex-col items-center">
+      <div className="flex flex-col gap-[32px] h-full items-center justify-center">
+        <div className="flex flex-col items-center gap-[8px]">
           <div className="font-semibold text-[89.76px] bg-gradient-to-r from-[#4356ff] to-[#283499] bg-clip-text text-transparent">
             PhysNova
           </div>
           <div className="text-3xl">
             Mastering IB Physics 5 questions at a time
           </div>
+          <button
+            className="border bg-[#4356ff] px-[16px] py-[8px] rounded-lg text-white"
+            onClick={() => handleLoginWithOAuth()}
+          >
+            Login with Google
+          </button>
         </div>
-        <button
-          className="border bg-[#4356ff] px-[16px] py-[8px] rounded-lg text-white"
-          onClick={() => handleLoginWithOAuth()}
-        >
-          Login with Google
-        </button>
+        <iframe
+          className="rounded-xl h-[400px] w-[600px] border"
+          src="https://www.loom.com/embed/be0684fa2f384aecba74000a180517f2?sid=6cc8af92-957b-4a19-b123-5cfd9cc4fb12"
+        />
       </div>
     </main>
   );
