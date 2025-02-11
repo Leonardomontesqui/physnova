a475d1777ebb91a40224cd2b231f44c10e906d19
Leonardo <lmontesq@uwaterloo.ca>
2025-02-11 15:13:51 -0500

    reducing unecessary code

--- app/provider.tsx
+++ app/provider.tsx
@@ -4,8 +4,8 @@ import { PostHogProvider } from "posthog-js/react";
 import { ReactNode } from "react";
 
 if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST as string,
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
     person_profiles: "always", // or 'always' to create profiles for anonymous users as well
   });
 }
